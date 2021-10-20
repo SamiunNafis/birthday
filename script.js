6324572797e16d20f728b62fcdd807bae627ac27
Samiun Nafis <luea099@gmail.com>
2021-10-20 15:54:23 +0600

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+/*
+ * Copyright (c) 2021 Samiun Nafis (SamiunNafis.github.io). All rights reserved.
+ *
+ * License under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by the applicable law or agreed in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 let q = new URLSearchParams(window.location.search).get('name')
-q = q || "User"
+q = q || "user"
+//Make first letter of the name uppercase
+q = q.charAt(0).toUpperCase() + q.slice(1)
 let name = document.getElementById('name')
 name.innerText = q
 let balloons = document.querySelectorAll('.balloon')
